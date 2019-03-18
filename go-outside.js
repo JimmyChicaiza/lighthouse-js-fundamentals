bfca44e49c22d68457aa5b68d9de5d5275b708d4
Jimmy Chicaiza <jimmy.chicaiza@gmail.com>
2019-03-17 23:37:23 -0400

    Adds conditionals exercises

--- go-outside.js
+++ go-outside.js
@@ -1,3 +1,21 @@
+/*
+var raining = false;
+var cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+*/
+
+
+
+
 var temperature = 2;
 
 
